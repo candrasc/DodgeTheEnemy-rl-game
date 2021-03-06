2b20736d2d628786eabd5440173f081135128d46
candrasc <charles.andrascik5@gmail.com>
2021-03-06 17:27:41 -0500

    Refactor enemies to inherit a parent NonPlayerObject class

--- objects/players.py
+++ objects/players.py
@@ -4,16 +4,11 @@ import copy
 class Player:
     def __init__(self, player_number=1, player_size = 30, step_size = 0.3, position = (1,1)):
 
-        if player_number == 1:
-            self.player = pygame.image.load("objects/object_images/player_one.png").convert()
-        else:
-            self.player = pygame.image.load("objects/object_images/player_two.png").convert()
-
+        self.player = pygame.image.load("objects/object_images/player_one.png").convert()
         self.player_number = player_number
         self.size = player_size
         self.step_size = step_size
         self.player = pygame.transform.scale(self.player,(self.size, self.size))
-        self._rect = self.player.get_rect()
         self.position = position
 
 
@@ -44,17 +39,14 @@ class Player:
                 copyobj.__dict__[name] = copy.deepcopy(attr)
         return copyobj
 
-class Enemy:
+class NonPlayerBase:
     """
-    Object to avoid in game
+    Base class for all objects that are not a player (rewards and enemies)
     """
     def __init__(self, size = 30, step_size = 0.3, starting_pos = (1,1), velocity=(1,1)):
 
         self.size = size
         self.step_size = step_size
-        self.enemy = pygame.image.load("objects/object_images/enemy.png").convert()
-        self.enemy = pygame.transform.scale(self.enemy,(self.size, self.size))
-        self._rect = self.enemy.get_rect()
         self.position = starting_pos
         self.velocity = velocity
 
@@ -86,3 +78,23 @@ class Enemy:
             else:
                 copyobj.__dict__[name] = copy.deepcopy(attr)
         return copyobj
+
+class Enemy(NonPlayerBase):
+    """
+    Object to avoid in game
+    """
+    def __init__(self,  size = 30, step_size = 0.3, starting_pos = (1,1), velocity=(1,1)):
+        super().__init__(size, step_size, starting_pos, velocity)
+        self.enemy = pygame.image.load("objects/object_images/enemy.png").convert()
+        self.enemy = pygame.transform.scale(self.enemy,(self.size, self.size))
+
+
+class Reward:
+    """
+    Items the player is trying to collect for points
+    """
+    def __init__(self, reward_value = 10, size = 30, step_size = 0.3, starting_pos = (1,1), velocity=(1,1)):
+        super().__init__(size, step_size, starting_pos, velocity)
+        self.reward = pygame.image.load("objects/object_images/reward_one.png").convert()
+        self.reward = pygame.transform.scale(self.reward, (self.size, self.size))
+        self.reward_value = reward_value
